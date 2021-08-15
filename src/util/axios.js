94d203c76b2e6c26e68f81fe9b178566669c253e
Ankur Kunal <mrankur801@gmail.com>
2021-08-15 08:36:17 +0530

    remove URL

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const URL = "http://localhost:8000";
+// const URL = "http://localhost:8000";
+
+const URL = "/";
 axios.defaults.withCredentials = true;
 export default axios.create({
   baseURL: URL,
